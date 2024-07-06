b429b8fe582b6f35e47034685ac955bb81d78b64
Janielle Jackson <jmjackson81303@gmail.com>
2024-07-06 19:51:17 -0400

    Replaced line 2 and 7 with modified code from nativewind

--- app/index.jsx
+++ app/index.jsx
@@ -1,22 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-white">
       <Text>janyellol</Text>
       <StatusBar style="auto"/>
       <Link href="/profile" style={{color: 'lightpink' }}>Go to Profile</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
