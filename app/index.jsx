80a36e88d16d49894462d27c0f29281481df0db8
Janielle Jackson <jmjackson81303@gmail.com>
2024-11-19 15:26:53 -0500

    update

--- app/index.jsx
+++ app/index.jsx
@@ -4,10 +4,10 @@ import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>janyellol</Text>
+    <View className="flex-1 items-center justify-center bg-red">
+      <Text className="text-3xl">TerpConnect</Text>
       <StatusBar style="auto"/>
-      <Link href="/profile" style={{color: 'lightpink' }}>Go to Profile</Link>
+      <Link href="/profile" style={{color: 'red' }}>Log In</Link>
     </View>
   );
 }
